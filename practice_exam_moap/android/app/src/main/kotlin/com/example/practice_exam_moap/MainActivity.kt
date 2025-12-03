86ebd76a85239c2d2d31ed3a18bd0afc99f623db
LxLS23 <20223tn120@utez.edu.mx>
2025-12-03 12:58:11 -0600

    [FIRST COMMIT] IC

---
+++ practice_exam_moap/android/app/src/main/kotlin/com/example/practice_exam_moap/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practice_exam_moap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ practice_exam_moap/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'branch_map_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Google Maps Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
+        useMaterial3: true,
+      ),
+      home: const BranchMapPage(),
+    );
+  }
+}
